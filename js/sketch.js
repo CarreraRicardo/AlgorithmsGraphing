4345213e1cb4ae66fc61c0459f38490466b6ae81
Ricardo Carrera <ricardo@internetpowerhotel.com>
2019-12-05 11:00:23 -0700

    Revert "Añadio el archivo circle"
    This reverts commit 8ab2e2dc695f3881691d24bf71a8697179a829f7.

--- js/sketch.js
+++ js/sketch.js
@@ -1,50 +1,47 @@
-function setup() {
 
-    createCanvas(500, 500);
+function setup(){
+
+	createCanvas(500,500);
 
 }
 
 
-function draw() {
+function draw(){
 
-    noLoop();
-    background('GREEN');
+	noLoop();
+	background('GREEN');
 
+	
+	//strokeWeight(1);
+	stroke('yellow');
+	/*
+	Direct({x1:0, y1: 0, x2: width, y2: height}); //   \ 
 
-    //strokeWeight(1);
-    stroke('yellow');
-    /*
-    Direct({x1:0, y1: 0, x2: width, y2: height}); //   \ 
+	stroke('blue');
+	Direct({x1:width, y1: 0, x2: 0, y2: height}); // / 
 
-    stroke('blue');
-    Direct({x1:width, y1: 0, x2: 0, y2: height}); // / 
+	stroke('red');
+	Direct({x1:0, y1: height / 2, x2: width, y2: width /2}); // --
 
-    stroke('red');
-    Direct({x1:0, y1: height / 2, x2: width, y2: width /2}); // --
+	stroke('purple');
+	Direct({x1:width/2, y1: 0, x2: width/2, y2: height}); // |
 
-    stroke('purple');
-    Direct({x1:width/2, y1: 0, x2: width/2, y2: height}); // |
 	*/
 
+	//Circle({radius:50,x:200,y:200});
 
-    Circle({
-        radius: 50,
-        x: 200,
-        y: 200
-    });
 
-    /*
-    var cursor = {
+	var cursor = {
 
-    	punto:{
-    		x:20,
-    		y:200
+		punto:{
+			x:20,
+			y:200
 
-    	},
-    	angulo:0
+		},
+		angulo:0
 
-    };
+	};
 
-    Koch(cursor,400,1);
-    */
+	Koch(cursor,400,1);
+	
 }
